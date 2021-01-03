5d34868a41cac1049e1aba01aafe99a57635d19d
Juuiko <mahonal@tcd.ie>
2021-01-03 14:46:07 +0000

    fixed white screen on page change

--- software-engineering/practical-visualisation/src/App.js
+++ software-engineering/practical-visualisation/src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import './App.css';
-import { Route, Switch, Router } from 'react-router-dom'
-import history from './backend/history'
+import './stylesheets/App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage'
 import UserPage from './pages/userpage'
 
 function App() {
   return (
-    <div className="App">
-      <Router history={history} basename=".">
+      <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route exact path="/user" component={UserPage}/>
+          <Route path="/" component={HomePage} exact/>
+          <Route path="/user" component={UserPage}/>
         </Switch>
-      </Router>
-    </div>
+      </BrowserRouter>
   )
 }
 
