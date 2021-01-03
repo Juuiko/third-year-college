c5bf6835bab8f16e2ac07284c4f95cb5a572ffe3
Juuiko <mahonal@tcd.ie>
2021-01-03 20:23:48 +0000

    added basic repo fetching

--- software-engineering/practical-measuring/main.go
+++ software-engineering/practical-measuring/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/google/go-github/github"
 )
 
 func main() {
-	fmt.Println("First commit!")
+	client := github.NewClient(nil)
+
+	user := "juuiko"
+	opt := &github.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
+
+	repos, _, err := client.Repositories.List(context.Background(), user, opt)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf(github.Stringify(repos))
 }
