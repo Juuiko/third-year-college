6d0889ffa708bfb3496186c6b98f29d37f4dd50a
Juuiko <mahonal@tcd.ie>
2021-01-04 15:06:25 +0000

    finish fetching pie chart data

--- software-engineering/practical-measuring/pieData.go
+++ software-engineering/practical-measuring/pieData.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
 
 //PieChart is the data type to make the final JSON the pie chart will use
 type PieChart struct {
@@ -8,9 +12,41 @@ type PieChart struct {
 	Use  int    `json:"value"`
 }
 
-func calcPieData([]Data) bool {
+func calcPieData(data []Data) bool {
 	//setup slice for pie chart value
 	pieData := make([]PieChart, 0)
-	fmt.Println(pieData)
+
+	dataFound := false
+
+	//for each repo
+	for i := range data {
+		//take each language used
+		for k, v := range data[i].languages {
+			//check if it's already in the final slice
+			for j := 0; j < len(pieData) && !dataFound; j++ {
+				//if it's found then add bytes
+				if pieData[j].Lang == k {
+					pieData[j].Use += v
+					dataFound = true
+				}
+			}
+			//if it isn't found, add it to the final slice
+			if dataFound == false {
+				pieData = append(pieData, PieChart{k, v})
+			} else {
+				dataFound = false
+			}
+		}
+
+	}
+	jsonData, err := json.Marshal(pieData)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(jsonData))
+	err = ioutil.WriteFile("pieStats.json", jsonData, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
 	return true
 }
