d121bd8b7e6853797b1ef3285efede491a5bf873
Juuiko <mahonal@tcd.ie>
2021-01-04 13:15:44 +0000

    added commit fetching and commented the codebase

--- software-engineering/practical-measuring/main.go
+++ software-engineering/practical-measuring/main.go
@@ -2,34 +2,54 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 
 	"github.com/google/go-github/github"
 )
 
 func main() {
+	//make github api client
 	client := github.NewClient(nil)
 
+	//pass user input later
 	user := "juuiko"
+
+	//repo list sorting
 	opt := &github.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
 
+	//get list of repos
 	repos, _, err := client.Repositories.List(context.Background(), user, opt)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	languages, _, err1 := client.Repositories.ListLanguages(context.Background(), "juuiko", repos[0].GetName())
-	if err1 != nil {
-		fmt.Println(err1)
-	}
+	//commit list options
+	clo := github.CommitsListOptions{Author: user}
 
-	jsonString, err := json.Marshal(languages)
-	fmt.Println(languages)
-	fmt.Println(string(jsonString))
-	err = ioutil.WriteFile("test.json", jsonString, 0644)
-	if err != nil {
-		fmt.Println(err)
+	//loop over every repo
+	for i := range repos {
+		//get map of languages used in every repo
+		rLanguages, _, err1 := client.Repositories.ListLanguages(context.Background(), user, repos[i].GetName())
+		if err1 != nil {
+			fmt.Println(err1)
+		}
+
+		//get a slice of commit objects by the user
+		rUserCommits, _, err2 := client.Repositories.ListCommits(context.Background(), user, repos[i].GetName(), &clo)
+		if err2 != nil {
+			fmt.Println(err2)
+		}
+		fmt.Println(rLanguages)
+		//get the date of each commit
+		for j := range rUserCommits {
+			fmt.Println(rUserCommits[j].GetCommit().GetAuthor().Date)
+		}
 	}
+	//jsonString, err := json.Marshal(languages)
+	//fmt.Println(languages)
+	//fmt.Println(string(jsonString))
+	//err = ioutil.WriteFile("test.json", jsonString, 0644)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
 }
